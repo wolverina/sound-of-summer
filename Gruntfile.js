308741cbd114e53f6578f6313b6414cfedea5d66
Chloe Weil <me@chloeweil.com>
2013-06-17 14:50:21 +0100

    add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,44 +4,49 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     
-    // uglify: {
-    //    options: {
-    //     mangle: false
-    //   },
-    //   my_target: {
-    //     files: {
-    //       'dist/assets/js/main.min.js': ['assets/js/bundle/*.js']
-    //     }
-    //   }
-    // },
+    uglify: {
+       options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'dist/assets/js/main.min.js': ['assets/js/bundle/*.js']
+        }
+      }
+    },
 
     grunticon: {
         icons: {
             options: {
               src: "src/assets/img/",
-              dest: "src/assets/css/icn"
+              dest: "dist/assets/css/icn"
             }
         }
     },
 
-    // targethtml: {
-    //   dist: {
-    //     files: {
-    //       'dist/site/snippets/footer.php': 'site/snippets/footer.php'
-    //     }
+    targethtml: {
+      dist: {
+        files: {
+          'dist/index.html': 'index.html'
+        }
+      }
+    },
+
+    // jshint: {
+    //   files: {
+    //     src: ['src/assets/js/bundle/main.js']
     //   }
-    // },
+    // }
 
   });
 
-  // Load the plugin that provides the "uglify" task.
-  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-grunticon');
-  //grunt.loadNpmTasks('grunt-targethtml');
+  grunt.loadNpmTasks('grunt-targethtml');
+ //grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task(s).
-  //grunt.registerTask('default', ['grunt-contrib-uglify','grunt-targethtml']);
-  grunt.registerTask('default', ['grunt-grunticon']);
+  grunt.registerTask('default', ['grunt-contrib-uglify','grunt-targethtml', 'grunt-grunticon']);
 
 
 };
